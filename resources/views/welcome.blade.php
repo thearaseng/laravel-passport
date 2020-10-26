06878490989568cee733c59b721f1b7aac842b87
Theara Seng <theara.seng1@wingmoney.com>
2020-10-26 20:53:51 +0700

    configure laravel passport

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
         <title>Laravel</title>
 
         <!-- Fonts -->
